867e8c82b674e678eec8bc9e87894a6dccb085a6
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-12-12 18:03:14 +0300

    Fourth commit

--- MvcCV/Controllers/DefaultController.cs
+++ MvcCV/Controllers/DefaultController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcCV.Models.Entity;
+using MvcCV.Models.Sınıf;
 
 namespace MvcCV.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        DbMvcCvEntities db = new DbMvcCvEntities();
         public ActionResult Index()
         {
-            return View();
+            Class1 cs = new Class1();
+            cs.Deger1 = db.TBLABOUT.ToList();
+            cs.Deger2 = db.TBLEXPERIENCE.ToList();
+            cs.Deger3 = db.TBLEDUCATION.ToList();
+            cs.Deger4 = db.TBLSKILLS.ToList();
+            cs.Deger5 = db.TBLINTERESTS.ToList();
+            cs.Deger6 = db.TBLAWARDS.ToList();
+            return View(cs);
+           // var degerler = db.TBLABOUT.ToList();
+           // return View(degerler);
         }
     }
 }
---
+++ MvcCV/Models/Sınıf/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcCV.Models.Entity;
+
+namespace MvcCV.Models.Sınıf
+{
+    public class Class1
+    {
+        public IEnumerable<TBLABOUT> Deger1 { get; set; }
+        public IEnumerable<TBLEXPERIENCE> Deger2 { get; set; }
+        public IEnumerable<TBLEDUCATION> Deger3 { get; set; }
+        public IEnumerable<TBLSKILLS> Deger4 { get; set; }
+        public IEnumerable<TBLINTERESTS> Deger5 { get; set; }
+        public IEnumerable<TBLAWARDS> Deger6 { get; set; }
+    }
+}
