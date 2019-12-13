a4279f9a048b69b7d8f3bf4694c2803e5cc5b0dd
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-12-13 11:06:36 +0300

    7th commit
    Adding skills and educations interface

---
+++ MvcCV/Controllers/EducationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCV.Models.Entity;
+using MvcCV.Models.Sınıf;
+
+namespace MvcCV.Controllers
+{
+    
+    public class EducationController : Controller
+    {
+        // GET: Education
+        DbMvcCvEntities db = new DbMvcCvEntities();
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            cs.Deger3 = db.TBLEDUCATION.ToList();
+            return View(cs);
+        }
+    }
+}
---
+++ MvcCV/Controllers/SkillsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCV.Models.Entity;
+using MvcCV.Models.Sınıf;
+
+namespace MvcCV.Controllers
+{
+    public class SkillsController : Controller
+    {
+        // GET: Skills
+        DbMvcCvEntities db = new DbMvcCvEntities();
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            cs.Deger4 = db.TBLSKILLS.ToList();
+            return View(cs);
+        }
+    }
+}
